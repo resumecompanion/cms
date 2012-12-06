2a40ed7b775ed7edfb9db03bacf282e5bca742f3
hellolucky <hellolucky123@gmail.com>
2012-12-06 17:12:32 +0800

    modify meta tag method

--- app/controllers/cms/pages_controller.rb
+++ app/controllers/cms/pages_controller.rb
@@ -1,7 +1,7 @@
 module Cms
   class PagesController < ApplicationController
     def index
-      url = Cms::Setting.find_by_key("global:index") || "/"
+      url = Cms::Setting.find_by_key("global:index")
       redirect_to url.value
     end
 
@@ -9,6 +9,10 @@ module Cms
       @page = Cms::Page.where(:slug => params[:id], :is_published => true).first
 
       redirect_to :action => :render_404 if @page.blank?
+
+      @title = @page.title || Cms::Setting.find_by_key("global:meta_title").try(:value)
+      @meta_description = @page.meta_description || Cms::Setting.find_by_key("global:meta_description").try(:value)
+      @meta_keywords = @page.meta_keywords || Cms::Setting.find_by_key("global:meta_keywords").try(:value)
     end
 
     def render_404
--- app/helpers/cms/application_helper.rb
+++ app/helpers/cms/application_helper.rb
@@ -1,17 +1,14 @@
 module Cms
   module ApplicationHelper
     def render_cms_page_title(title)
-     title = Cms::Setting.find_by_key("global:meta_title").value rescue "" if title.blank?
      content_tag("title", title)
     end
 
     def render_cms_meta_description(meta_description)
-      meta_description = Cms::Setting.find_by_key("global:meta_description").value rescue "" if meta_description.blank?
       tag(:meta, {:name => "description", :content => meta_description})
     end
 
     def render_cms_meta_keywords(meta_keywords)
-      meta_keywords = Cms::Setting.find_by_key("global:meta_keywords").value rescue "" if meta_keywords.blank?
       tag(:meta, {:name => "keywords", :content => meta_keywords})
     end
   end
