fee715c259501b18aaa488828b7c466544fb9726
hellolucky <hellolucky123@gmail.com>
2012-12-10 14:27:16 +0800

    add convert_macro method

--- app/helpers/cms/pages_helper.rb
+++ app/helpers/cms/pages_helper.rb
@@ -1,4 +1,25 @@
 module Cms
   module PagesHelper
+    def convert_macro(content)
+      doc = Nokogiri::HTML.fragment(content)
+
+      # ex: <children_count>home</children_count>
+      doc.css("children_count").each do |dom|
+        page = Cms::Page.select(:children_count).where(:slug => dom.content).first
+        dom.name = "span"
+        dom.content = "(#{page.children_count rescue 0})"
+      end
+
+      # ex: <children_pages>home</children_pages>
+      doc.css("children_pages").each do |dom|
+        page = Cms::Page.where(:slug => dom.content).first
+        dom.name = "div"
+        page.children.order("title").each do |child_page|
+          dom << link_to(child_page.title, cms.pages_path(child_page))
+        end
+      end
+
+      doc.inner_html
+    end
   end
 end
