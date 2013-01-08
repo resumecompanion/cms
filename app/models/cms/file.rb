d1d868ec2b22c54e62ea717ba0ab3476c3bcdeea
hellolucky <hellolucky123@gmail.com>
2013-01-08 11:49:42 +0800

    add filename field for image

--- app/models/cms/file.rb
+++ app/models/cms/file.rb
@@ -1,6 +1,6 @@
 module Cms
   class File < ActiveRecord::Base
-    attr_accessible :image, :title
+    attr_accessible :image, :title, :filename
 
     mount_uploader :image, Cms::ImageUploader
     paginates_per 48
@@ -8,5 +8,13 @@ module Cms
     validates_presence_of :image
 
     belongs_to :user, :foreign_key => :cms_user_id
+
+    def filename=(filename)
+      self.image.rename(filename) if filename.present?
+    end
+
+    def filename
+      ::File.basename(self.image.path).split(".").first
+    end
   end
 end
