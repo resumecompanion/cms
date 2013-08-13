8c7fd97fbe40e2e6030e6db6126866739f23f0c5
Josh Moore <joshsmoore@gmail.com>
2013-08-13 11:28:00 +0800

    redirect all non user or admin pages from https to http
    CMS-114

--- lib/cms/engine.rb
+++ lib/cms/engine.rb
@@ -1,7 +1,14 @@
+require 'rack/ssl-enforcer'
 module Cms
   class Engine < ::Rails::Engine
     isolate_namespace Cms
     
+    initializer :ssl do |app|
+      unless Rails.env.development?
+        app.middleware.use Rack::SslEnforcer, only: %r{/users/.*}
+        app.middleware.use Rack::SslEnforcer, only: %r{/admin/.*}, ignore: %r{/assets.*}, strict: true
+      end
+    end
     initializer :append_migrations do |app|
       unless app.root.to_s == root.to_s
         app.config.paths["db/migrate"] += config.paths["db/migrate"].expanded
