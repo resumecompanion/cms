bc37fd77725c561985f7fddca6d2f9b5fe7418fa
Josh Moore <joshsmoore@gmail.com>
2013-08-30 15:41:34 +0800

    moved the CMS asset compilation into engine

--- lib/cms/engine.rb
+++ lib/cms/engine.rb
@@ -7,6 +7,11 @@ module Cms
         app.config.paths["db/migrate"] += config.paths["db/migrate"].expanded
       end
     end
+    
+    initializer "cms.precompile_assets" do |app|
+      app.config.assets.precompile += %w( cms/application.js, cms/application.css cms/admin.js cms/admin.css )
+      app.config.assets.precompile += %w( cms/plugin/ckeditor/init.js cms/plugin/ckeditor/*)
+    end
 
   end
 end
